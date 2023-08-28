b4f7683d9675ce1c5e12be5b7ec8cfcce29ce8ef
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-28 16:00:50 -0500

    second exercise

---
+++ 02-intro-javascript/src/bases/01-index-const-let.js
@@ -0,0 +1,18 @@
+
+
+const name = "Johnatan";
+let last_name = "Ortiz"
+
+let valueDado = 5;
+valueDado = 4;
+
+console.log( name, last_name, valueDado )
+
+if ( true ) {
+    const name = "Steven"
+    let valueDado = 6;
+
+    console.log(name)
+}
+
+console.log( valueDado )
